b023c5187753f10ef6c88dc5a6e4edd82818e743
CatMan <garfield_lv@163.com>
2019-07-25 11:42:22 +0800

    tree script

---
+++ .tree.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+filename="./README.md"
+echo '# Java基础案例练习' > ${filename}
+
+
+for p in `find ./* -maxdepth 1 -name "Test*.java"`;
+    do
+        # echo $p
+        index=`cat ${p}|sed -n '3p'`
+        echo "- [${p}](${p})\n   ${index}" >>${filename}
+    done
+
+echo "ok!"
